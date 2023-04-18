494ca6b259896a38c10ef3c0377ae2f6e132d229
Mehmet Salgar <salgarm@gmx.de>
2023-04-18 12:54:08 +0200

    Testing Docker Images

--- src/main/java/org/salgar/akka/fsm/foureyes/creditscore/CreditScoreApplication.java
+++ src/main/java/org/salgar/akka/fsm/foureyes/creditscore/CreditScoreApplication.java
@@ -1,11 +1,14 @@
 package org.salgar.akka.fsm.foureyes.creditscore;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
+@Slf4j
 public class CreditScoreApplication {
     public static void main(String[] args) {
+        log.info("Credit Score Application is starting!");
         new SpringApplicationBuilder(CreditScoreApplication.class)
                 .registerShutdownHook(true)
                 .run(args);
